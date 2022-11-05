a799c853a9955ad00bbcf9d9d91007943b0e3b19
ranynigth <rany4ka@gmail.com>
2022-11-05 17:59:31 +0300

    fix: task 3 fake extensions

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -6,16 +6,18 @@ const secret_folder = path.join(__dirname, 'secret-folder');
 fs.readdir(secret_folder, 'utf8', (err, files) => {
     if (err) throw err;
     files.forEach(file =>  {
-        const name = path.basename(file, `${path.extname(file)}`)
         const ext =path.extname(file)
+        const name = path.basename(file, ext);
         fs.stat(path.join(__dirname, 'secret-folder', `${file}`), (err, stats) => {
             let size = (stats.size / 1024).toFixed(2) + ' kb'
-            if (err) {
-              console.error(err)
-              return
+            switch (stats.isFile()) {
+              case true:
+                  console.log(`${name} - ${ext.slice(1)} - ${size}`);
+                  break;
+              case false:
+                  break;
             }
-            console.log(`${name} - ${ext.slice(1)} - ${size}`)
-          })
+        })
     })
     
   });
